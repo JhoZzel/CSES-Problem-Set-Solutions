d55c71b78c172761491650bef4dd5a9dacc2dd79
Joel Jhotan Chavez Chico <95269819+JhoZzel@users.noreply.github.com>
2024-01-19 18:24:34 -0500

    Create MissingNumber.cpp

---
+++ 01. Introductory Problems/MissingNumber.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main() {
+    int n; cin >> n;
+    vector<bool> vis(n);
+    for (int i = 0; i < n - 1; i++) {
+        int x; cin >> x;
+        vis[x - 1] = true;
+    }
+    for (int i = 0; i < n; i++) {
+        if (vis[i]) continue;
+        cout << i + 1 << "\n";
+        break;
+    }
+    return 0;
+}
