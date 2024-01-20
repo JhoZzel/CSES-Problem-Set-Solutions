cf13f77e3867124944c123ab2acb007566343e05
Joel Jhotan Chavez Chico <95269819+JhoZzel@users.noreply.github.com>
2024-01-19 19:28:14 -0500

    Update Number_Spiral.cpp

--- 01. Introductory Problems/Number_Spiral.cpp
+++ 01. Introductory Problems/Number_Spiral.cpp
@@ -3,15 +3,15 @@ using namespace std;
 int main() {
     int t; cin >> t;
     while(t--) {
-        ll x, y;
+        long long x, y;
         cin >> x >> y;
         if (x > y) {
-            if (x & 1) cout << x * (x - 1) + 1 - (x - y);
-            else cout << x * x - y + 1;
+            if (x & 1) cout << x * (x - 1) + 1 - (x - y) << "\n";
+            else cout << x * x - y + 1 << "\n";
         }
         else {
-            if (y & 1) cout << y * y - x + 1;
-            else cout << y * (y - 1) + 1 - (y - x);
+            if (y & 1) cout << y * y - x + 1 << "\n";
+            else cout << y * (y - 1) + 1 - (y - x) << "\n";
         }
     }
     return 0;
