0118a1f3673ddd701e5c9604ce9c8e843a9d0c79
Joel Jhotan Chavez Chico <95269819+JhoZzel@users.noreply.github.com>
2024-01-19 19:31:20 -0500

    Create Two_Knights.cpp

---
+++ 01. Introductory Problems/Two_Knights.cpp
@@ -0,0 +1,11 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main() {
+    int n; cin >> n;
+    for (int k = 1; k <= n; k++) {
+        long long total = 1ll * k * k * (k * k - 1) / 2;
+        long long atack = 4 * (k - 1) * (k - 2);
+        cout << total - atack << "\n";
+    }
+    return 0;
+}
