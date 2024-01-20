b3573a7e0aac60b0fdfc6993dd9ba6cdc3baa8d1
Joel Jhotan Chavez Chico <95269819+JhoZzel@users.noreply.github.com>
2024-01-20 13:54:43 -0500

    Update and rename a.cpp to Dice_Combinations.cpp

---
+++ 03. Dynamic Programming/Dice_Combinations.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+const int N = 1e6+ 5;
+const int MOD = 1e9 + 7;
+
+int main() {
+    int n; cin >> n;
+    int dp[N] = {};
+    dp[0] = 1;
+    for (int k = 1; k <= n; k++) {
+        for (int d = 1; d <= 6; d++) {
+            if (k - d < 0) break;
+            dp[k] = (dp[k] + dp[k - d]) % MOD;
+        }
+    }
+    cout << dp[n] << "\n";
+}
