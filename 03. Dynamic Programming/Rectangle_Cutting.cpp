13fe5ca5050f4e22369a79aef8da9e8e24bd583e
Joel Jhotan Chavez Chico <95269819+JhoZzel@users.noreply.github.com>
2024-01-21 14:40:03 -0500

    Update Rectangle_Cutting.cpp

--- 03. Dynamic Programming/Rectangle_Cutting.cpp
+++ 03. Dynamic Programming/Rectangle_Cutting.cpp
@@ -5,13 +5,11 @@ const int INF = 1e9 + 5;
 const int N = 5e2 + 5;
  
 int dp[N][N];
-bool vis[N][N];
  
 int DP(int a, int b) {
     if (a == b) return 0;
     if (a < b) swap(a ,b);
-    if (vis[a][b]) return dp[a][b];
-    vis[a][b] = true;
+    if (~dp[a][b]) return dp[a][b];
     dp[a][b] = INF;
     for (int x = 1; x < a; x++) {
         dp[a][b] = min(dp[a][b], 1 + DP(a - x, b) + DP(x, b));
@@ -25,6 +23,7 @@ int DP(int a, int b) {
 int main() {
     int n,m;
     cin >> n >> m;
+    memset(dp, -1, sizeof(dp));
     cout << DP(n, m) << "\n";
     return 0;
 }
