a56e32b992d417ece0a7d35a343ed17d552ccda6
Joel Jhotan Chavez Chico <95269819+JhoZzel@users.noreply.github.com>
2024-02-12 02:39:16 -0500

    Create Shortest Routes I.cpp

---
+++ 04. Graph Algorithms/Shortest Routes I.cpp
@@ -0,0 +1,50 @@
+#include <bits/stdc++.h>
+#define fast_io ios_base::sync_with_stdio(0); cin.tie(0)
+#define dbg(x) do {cerr << #x <<" = " << (x) << endl; } while (false)
+using namespace std;
+typedef long long ll;
+
+const int N = 1e5 + 5;
+const ll INF = 1e18 + 5;
+
+int n;
+int m;
+ll D[N];
+vector<pair<int,int>> G[N];
+
+int main() {
+    fast_io;
+    cin >> n >> m;
+    for (int i = 0; i < m; i++) {
+        int u,v,w;
+        cin >> u >> v >> w;
+        u--; v--;
+        G[u].emplace_back(v,w);
+        
+    }
+    
+    priority_queue<pair<ll,int>, vector<pair<ll,int>>, greater<pair<ll,int>>> Q;
+    
+    fill(D, D + n, INF);
+    D[0] = 0;
+    Q.emplace(0,0);
+    
+    while(!Q.empty()) {
+        int u;
+        ll d;
+        tie(d,u) = Q.top(); Q.pop();
+        if (d != D[u]) continue;
+        for (auto [v, w] : G[u]) {
+            if (D[u] + w < D[v]) {
+                D[v] = D[u] + w;
+                Q.emplace(D[v], v);
+            }
+        }
+    }
+    
+    for (int i = 0; i < n; i++) {
+        cout << D[i] << " ";
+    }
+    
+    return 0;
+}
