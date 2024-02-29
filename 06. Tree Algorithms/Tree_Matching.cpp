db7be7b62921cabb1f57fc1bef8257ee21990f2c
Joel Jhotan Chavez Chico <95269819+JhoZzel@users.noreply.github.com>
2024-02-28 21:01:01 -0500

    Update Tree_Matching.cpp

--- 06. Tree Algorithms/Tree_Matching.cpp
+++ 06. Tree Algorithms/Tree_Matching.cpp
@@ -1,39 +1,30 @@
 #include <bits/stdc++.h>
 using namespace std;
-
+ 
 const int N = 2e5 + 5;
-
+ 
 int n;
-int par[N];
 int dp[N][2];
 vector<int> G[N];
-
-void get_parents(int u, int p) {
-    for (int v : G[u]) {
-        if (v == p) continue;
-        par[v] = u;
-        get_parents(v, u);
-    }
-}
-
-int DFS(int u, bool on) {
+ 
+int DFS(int u, int p, bool on) {
     if (~dp[u][on]) return dp[u][on];
     int all = 0;
     for (int v : G[u]) {
-        if (v == par[u]) continue;
-        all += DFS(v, 0);
+        if (v == p) continue;
+        all += DFS(v, u, 0);
     } 
     
     if (on) return dp[u][on] = all;
     
     dp[u][on] = all;
     for (int v : G[u]) {
-        if (v == par[u]) continue;
-        dp[u][on] = max(dp[u][on], all - DFS(v, 0) + 1 + DFS(v, 1));
+        if (v == p) continue;
+        dp[u][on] = max(dp[u][on], all - DFS(v, u, 0) + 1 + DFS(v, u, 1));
     }
     return dp[u][on];
 }
-
+ 
 int main() {
     memset(dp, -1, sizeof(dp));
     cin >> n;
@@ -44,8 +35,6 @@ int main() {
         G[u].push_back(v);
         G[v].push_back(u);
     }
-    par[0] = -1;
-    get_parents(0,-1);
-    cout << DFS(0, 0) << "\n";
+    cout << DFS(0, -1, false) << "\n";
     return 0;
 }
