b0d1208a651a8bc42c583d596022276a79af79db
Joel Jhotan Chavez Chico <95269819+JhoZzel@users.noreply.github.com>
2024-03-24 20:32:31 -0500

    Update Book_Shop.cpp

--- 03. Dynamic Programming/Book_Shop.cpp
+++ 03. Dynamic Programming/Book_Shop.cpp
@@ -1,27 +1,21 @@
 #include <bits/stdc++.h>
 using namespace std;
  
-const int MOD = 1e9 + 7;
-const int N = 1e3 + 5;
-const int M = 1e5 + 5;
-
-int dp[N][M];
-
 int main() {
-    int n, x; 
+    int n, x;
     cin >> n >> x;
     vector<int> h(n), s(n);
     for (int &e : h) cin >> e;
     for (int &e : s) cin >> e;
-    for (int val = 0; val < M; val++) dp[n][val] = 0;
-    for (int pos = n - 1; pos >= 0; pos--) {
-        for (int val = 0; val <= x; val++) {
-            dp[pos][val] = dp[pos + 1][val];
-            if (val - h[pos] >= 0) {
-                dp[pos][val] = max(dp[pos + 1][val], dp[pos + 1][val - h[pos]] + s[pos]);
-            }
+    vector<int> last(x + 1);
+    for (int pos = 0; pos < n; ++pos) {
+        vector<int> cur(x + 1);
+        for (int cap = 0; cap <= x; ++cap) {
+            cur[cap] = last[cap];
+            if (cap >= h[pos]) cur[cap] = max(last[cap], last[cap - h[pos]] + s[pos]);
         }
+        swap(last, cur);
     }
-    cout << dp[0][x] << "\n";
+    cout << last[x] << "\n";
     return 0;
 }
