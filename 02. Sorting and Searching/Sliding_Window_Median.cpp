5f284a97826672199b41f0d2cd31836e9b556487
Joel Jhotan Chavez Chico <95269819+JhoZzel@users.noreply.github.com>
2024-05-15 01:10:38 -0500

    Update Sliding_Window_Median.cpp

--- 02. Sorting and Searching/Sliding_Window_Median.cpp
+++ 02. Sorting and Searching/Sliding_Window_Median.cpp
@@ -1,15 +1,12 @@
 #include <bits/stdc++.h>
 using namespace std;
-typedef long long ll;
-
-const int INF = 1e9 + 9;
-
+ 
 struct DynamicMedianMS {
     multiset<int> ms;
     multiset<int>::iterator med_it;
     int med_idx;
  
-    DynamicMedianMS() : ms({-INF}), med_it(ms.begin()), med_idx(0) {}
+    DynamicMedianMS() : ms({-1}), med_it(ms.begin()), med_idx(0) {}
   
     void insert(int x) {
         if(x < *med_it) med_idx++; // correct the index
@@ -19,7 +16,7 @@ struct DynamicMedianMS {
         if(med_idx > idx) med_it--;
         med_idx = idx;
     }
-
+ 
     void remove(int x) {
         if (x <= *med_it) med_idx--; // correct the index
         if (ms.find(x) == med_it) {
@@ -41,40 +38,21 @@ struct DynamicMedianMS {
         return *med_it;
     }
 };
-
+ 
 int main() {
     ios_base::sync_with_stdio(0);
     cin.tie(0); cout.tie(0);
-    
+    int n,k;
+    cin >> n >> k;
+    vector<int> a(n);
+    for (int &e : a) cin >> e;
     DynamicMedianMS DMS;
-    
-    ll fb = 0;
-    ll fa = 0;
-
-    int q; cin >> q;
-    for (int i = 0; i < q; i++) {
-        int op; cin >> op;
-        if (op == 1) {
-            int a,b;
-            cin >> a >> b;
-            fb += b;
-            if (i == 0) {
-                DMS.insert(a);
-                continue;
-            }
-            int med_ant = DMS.get();
-            DMS.insert(a);
-            int med_cur = DMS.get();
-            
-            int delta = abs(med_ant - med_cur);
-            if (med_ant < med_cur and DMS.ms.size() & 1) fa -= delta;
-            if (med_cur < med_ant and DMS.ms.size() & 1) fa += delta;
-            
-            fa += abs(a - med_cur);
-        }
-        else {
-            cout << DMS.get() << ' ' << fa + fb << '\n';
-        }
+    for (int i = 0; i < k; i++) DMS.insert(a[i]);
+    cout << DMS.get() << ' ';
+    for (int i = k; i < n; i++) {
+        DMS.insert(a[i]);
+        DMS.remove(a[i - k]);
+        cout << DMS.get() << ' ';
     }
     return 0;
 }
