f2afdd2db5b5fe0cd4220abd3c3723b12a44bec0
Joel Jhotan Chavez Chico <95269819+JhoZzel@users.noreply.github.com>
2024-08-14 09:32:54 -0500

    Update Room_Allocation.cpp

--- 02. Sorting and Searching/Room_Allocation.cpp
+++ 02. Sorting and Searching/Room_Allocation.cpp
@@ -1,6 +1,10 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+#define all(x) x.begin(), x.end()
+#define ff first
+#define ss second
+ 
 int main() {
     int n; cin >> n;
     vector<tuple<int,int,int>> a;
@@ -9,26 +13,25 @@ int main() {
         cin >> L >> R;
         a.emplace_back(L, R, i);
     }
-    sort(a.begin(), a.end());
-
-    priority_queue<pair<int,int>, vector<pair<int,int>>, greater<pair<int,int>>> pq;    
+    sort(all(a));
+    multiset<pair<int,int>> ms; // R - room_id
     int k = 0;
     vector<int> rooms(n);
     for (auto [L, R, i] : a) {
-        if (pq.empty() || L <= pq.top().first) {
-            pq.emplace(R, k);
+        if (ms.empty() or L <= ms.begin()->ff) {
+            ms.emplace(R, k);
             rooms[i] = k;
             k++;
         }
         else {
-            int room = pq.top().second;
-            pq.pop();
-            pq.emplace(R, room);
+            int room = ms.begin()->ss;
+            ms.erase(ms.begin());
+            ms.emplace(R, room);
             rooms[i] = room;
         }
     }
     cout << k << "\n";
-    for (int r : rooms) cout << r + 1 << " ";
-
+    for (int e : rooms) cout << e + 1 << " ";
+    
     return 0;
 }
