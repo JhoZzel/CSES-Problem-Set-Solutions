60afb98b41c9fbf267de0427819c7a30c13d1813
Joel Jhotan Chavez Chico <95269819+JhoZzel@users.noreply.github.com>
2025-01-13 20:49:14 -0500

    Update Stick_Lengths.cpp

--- 02. Sorting and Searching/Stick_Lengths.cpp
+++ 02. Sorting and Searching/Stick_Lengths.cpp
@@ -1,27 +1,26 @@
 #include <bits/stdc++.h>
 using namespace std;
-
+ 
 using ll = long long;
-
+ 
 int main() {
     cin.tie(0) -> sync_with_stdio(0);
     
     int n; cin >> n;
-    vector<int> a(n + 1);
-    vector<ll> pre(n + 1);
-    for (int i = 1; i <= n; i++) cin >> a[i];
+    vector<int> a(n);
+    for (int &e : a) cin >> e;
     sort(a.begin(), a.end());
-
-    ll total = accumulate(a.begin(), a.end(), 0ll);
+ 
     ll sa = 0;
+    ll total = accumulate(a.begin(), a.end(), 0ll);
     ll ans = LLONG_MAX;
-    for (int i = 1; i <= n; i++) {
-        int L = i - 1;
-        int R = n - i;
+    for (int i = 0; i < n; i++) {
+        int L = i;
+        int R = n - i - 1;
         ans = min(ans, total + 1ll * a[i] * (L - R - 1) - 2 * sa);
         sa += a[i];
     }
     cout << ans << '\n';
-
+ 
     return 0;
 }
