1cb0b36c412a261f39caf3c6c680a45faa94104c
Joel Jhotan Chavez Chico <95269819+JhoZzel@users.noreply.github.com>
2025-02-09 15:28:18 -0500

    Update Tasks_and_Deadlines.cpp

--- 02. Sorting and Searching/Tasks_and_Deadlines.cpp
+++ 02. Sorting and Searching/Tasks_and_Deadlines.cpp
@@ -1,24 +1,18 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-using ll = long long;
-
-int main() {    
+int main() {
     int n; cin >> n;
-    vector<pair<int,int>> a;
-    for (int i = 0; i < n; i++) {
-        int x,y;
-        cin >> x >> y;
-        a.emplace_back(x, y);
-    }
+    vector<pair<int,int>> a(n);
+    for (auto &[x, y] : a) cin >> x >> y;
     sort(a.begin(), a.end());
 
-    ll ans = 0, t = 0;
+    long long ans = 0, t = 0;
     for (auto [x, y] : a) {
         t += x;
         ans += y - t;
     }
     cout << ans << '\n';
-  
+
     return 0;
 }
